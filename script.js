a8de7b4d3d5188b6b24b74417247bdad8fd8ee01
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-03-08 11:57:06 -0300

    update

---
+++ script.js
@@ -0,0 +1,50 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector('[data-board]');
+
+let isCircleTurn;
+
+const startGame = () => {
+  for (const cell of cellElements) {
+    cell.addEventListener('click', handleClick, {once:true});
+  }
+
+  isCircleTurn = false;
+
+  board.classList.add('x');
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+board.classList.remove('circle');
+board.classList.remove('x');
+
+if (isCircleTurn) {
+  board.classList.add('circle');
+} else {
+  board.classList.add('x');
+}
+};
+
+const handleClick = (e) => {
+  // Colocar a marca (X ou círculo)
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? 'circle' : 'x';
+  placeMark(cell, classToAdd);
+  
+  // Verificar por vitória
+
+
+
+  
+  // Verificar por empate
+  
+  // Mudar símbolo
+  swapTurns();
+  };
+
+startGame();
