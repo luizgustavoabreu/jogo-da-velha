d653ee0046374cf5d1a73e5b0b40df48dddcf31e
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-03-09 12:37:04 -0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector('[data-board]');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const winningMessage = document.querySelector('[data-winning-message]');
 
 let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const startGame = () => {
   for (const cell of cellElements) {
     cell.addEventListener('click', handleClick, {once:true});
-  }
+  };
 
   isCircleTurn = false;
 
   board.classList.add('x');
+};
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn ? "O Venceu!" : "X Venceu!";
+  }
+  winningMessage.classList.add('show-winning-message');
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
 };
 
 const placeMark = (cell, classToAdd) => {
@@ -34,11 +64,15 @@ const handleClick = (e) => {
   // Colocar a marca (X ou círculo)
   const cell = e.target;
   const classToAdd = isCircleTurn ? 'circle' : 'x';
+
   placeMark(cell, classToAdd);
   
   // Verificar por vitória
+const isWin = checkForWin(classToAdd);
 
-
+if (isWin) {
+  endGame(false);
+};
 
   
   // Verificar por empate
